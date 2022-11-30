bac01f6bab4b8c6f301f06e00f5a1ef2fac22c6b
Tiffany Marko <tiffanyjmarko@gmail.com>
2022-11-29 18:58:25 -0800

    push with error

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
+const installPrompt = null 
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    installPrompt = event
+   console.log(event)
+     
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    installPrompt.prompt()
+    installPrompt.userChoice.then((choice)=>{
+        console.log(choice.outcome)
+        if (choice.outcome === "accepted") {
+            installPrompt = null
+        }
+    })
+    if (installPrompt !== null){
+ 
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log("user installed app")
+});
